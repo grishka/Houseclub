3ba052d076a83eeb73c6f76307f081ffe5beb28d
Denys Nykyforov <popalay1@gmail.com>
2021-02-25 23:30:21 +0800

    Add ability to mute speakers

--- Houseclub/src/main/java/me/grishka/houseclub/notification/NotificationHandlerBroadcastReceiver.java
+++ Houseclub/src/main/java/me/grishka/houseclub/notification/NotificationHandlerBroadcastReceiver.java
@@ -10,13 +10,16 @@ import me.grishka.houseclub.VoiceService;
 
 public class NotificationHandlerBroadcastReceiver extends BroadcastReceiver {
     public static final String ACTION_LEAVE_ROOM = "ACTION_LEAVE_ROOM";
+    public static final String ACTION_TOGGLE_MUTE_SPEAKER = "ACTION_TOGGLE_MUTE_SPEAKER";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        VoiceService svc=VoiceService.getInstance();
+        if(svc==null)return;
         if (Objects.equals(intent.getAction(), ACTION_LEAVE_ROOM)) {
-            if (VoiceService.getInstance() != null) {
-                VoiceService.getInstance().leaveCurrentChannel();
-            }
+            svc.leaveCurrentChannel();
+        }else if (Objects.equals(intent.getAction(), ACTION_TOGGLE_MUTE_SPEAKER)) {
+            svc.setSpeakerMuted(!svc.isSpeakerMuted());
         }
     }
 }
